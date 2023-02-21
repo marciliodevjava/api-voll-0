69873624a522ff88e30b4b15b6dc6bd685266c5e
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-21 12:54:27 -0300

    Criação AutenticaçãoService

--- src/main/java/br/com/med/voll/api/repository/UsuarioRepository.java
+++ src/main/java/br/com/med/voll/api/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package br.com.med.voll.api.repository;
 
 import br.com.med.voll.api.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }

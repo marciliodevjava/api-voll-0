a043d905a190d2ac2fd98691f847d5a9ad4a51d1
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-21 13:40:16 -0300

    Add SecurityConfigurations e desabilitando a autenticação via navegador e liberando no insonia

--- src/main/java/br/com/med/voll/api/configuration/CorsConfiguration.java
+++ src/main/java/br/com/med/voll/api/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.med.voll.api.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;

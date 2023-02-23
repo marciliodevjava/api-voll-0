e238c33c33b040e721808dcdafac6880d1fdaa50
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-22 21:40:56 -0300

    Atualização dos metodos

--- src/main/java/br/com/med/voll/api/dto/PacienteRetornoCriadoDto.java
+++ src/main/java/br/com/med/voll/api/dto/PacienteRetornoCriadoDto.java
@@ -2,10 +2,11 @@ package br.com.med.voll.api.dto;
 
 import br.com.med.voll.api.domain.Endereco;
 import br.com.med.voll.api.domain.Paciente;
+import jakarta.validation.constraints.Pattern;
 
-public record PacienteRetornoCriadoDto(String nome, String email, String telefone,String cpf, Endereco endereco) {
+public record PacienteRetornoCriadoDto(Long id, String nome, String email, String telefone,String cpf, Endereco endereco) {
 
     public PacienteRetornoCriadoDto(Paciente paciente){
-        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
     }
 }

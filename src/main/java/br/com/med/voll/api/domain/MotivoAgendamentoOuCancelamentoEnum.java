ad14b94c1cd98333a7c549f3120cf19874c6a695
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-23 10:29:04 -0300

    Criacação da classe MotivoAgendamentoOuCancelamentoEnum é um enum com parametros a serem passados no body da requisição

---
+++ src/main/java/br/com/med/voll/api/domain/MotivoAgendamentoOuCancelamentoEnum.java
@@ -0,0 +1,9 @@
+package br.com.med.voll.api.domain;
+
+public enum MotivoAgendamentoOuCancelamentoEnum {
+    CANCELAMENTO,
+    DESISTENCIA,
+    MOTIVO_PESSOAIS,
+    VIAGEM,
+    CONSULTA_MARCADA,
+}

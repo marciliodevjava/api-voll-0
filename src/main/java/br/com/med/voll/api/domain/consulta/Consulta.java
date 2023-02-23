9ba6125a813c64eb48293cd5a4ede8e39e78f9d6
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-23 10:29:59 -0300

    CriacAtualização de metodos

--- src/main/java/br/com/med/voll/api/domain/consulta/Consulta.java
+++ src/main/java/br/com/med/voll/api/domain/consulta/Consulta.java
@@ -1,8 +1,10 @@
 package br.com.med.voll.api.domain.consulta;
 
 import br.com.med.voll.api.domain.Medico;
+import br.com.med.voll.api.domain.MotivoAgendamentoOuCancelamentoEnum;
 import br.com.med.voll.api.domain.Paciente;
-import br.com.med.voll.api.dto.DadosConsultasDto;
+import br.com.med.voll.api.dto.CancelamentoDetalheConsutaDto;
+import br.com.med.voll.api.exception.medico.ValidacaoException;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -26,8 +28,10 @@ public class Consulta {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "paciente_id")
     private Paciente paciente;
-
     private LocalDateTime data;
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private MotivoAgendamentoOuCancelamentoEnum status = MotivoAgendamentoOuCancelamentoEnum.CONSULTA_MARCADA;
 
     public Consulta(Long idMedico, Long idPaciente, LocalDateTime data){
         this.medico = new Medico(idMedico);
@@ -35,4 +39,23 @@ public class Consulta {
         this.data = data;
     }
 
+    public boolean cancelar(MotivoAgendamentoOuCancelamentoEnum cancelamento) {
+        if(cancelamento.equals(MotivoAgendamentoOuCancelamentoEnum.VIAGEM)){
+            System.out.println("Motivo cancelamento autorizado");
+        } else if(cancelamento.equals(MotivoAgendamentoOuCancelamentoEnum.MOTIVO_PESSOAIS)){
+            System.out.println("Motivo cancelamento autorizado");
+        } else if(cancelamento.equals(MotivoAgendamentoOuCancelamentoEnum.DESISTENCIA)){
+            System.out.println("Motivo cancelamento autorizado");
+        } else if(cancelamento.equals(MotivoAgendamentoOuCancelamentoEnum.CANCELAMENTO)){
+            System.out.println("Motivo cancelamento autorizado");
+        } else {
+            throw new ValidacaoException("Motivo recusado");
+        }
+        this.status = cancelamento;
+        return true;
+    }
+
+    public void excluir(MotivoAgendamentoOuCancelamentoEnum cancelamento) {
+        this.status = cancelamento;
+    }
 }

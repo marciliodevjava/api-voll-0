543d0ea5ec05ee199c60b9b6c3d34d18888c47b2
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-24 09:59:14 -0300

    Criação de inclass para Validar o HPaciente tem uma consulta naquele mesmo horário

---
+++ src/main/java/br/com/med/voll/api/util/validacoes/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,29 @@
+package br.com.med.voll.api.util.validacoes;
+
+import br.com.med.voll.api.dto.DadosConsultasDto;
+import br.com.med.voll.api.exception.medico.ValidacaoException;
+import br.com.med.voll.api.repository.ConsultaRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorPacienteSemOutraConsultaNoDia implements ValidadorAgendamentoDeConsulta {
+
+    @Autowired
+    private ConsultaRespository consultaRespository;
+
+    public void validar(DadosConsultasDto dadosConsultasDto) {
+
+        LocalDateTime primeiroHorario = dadosConsultasDto.data().withHour(7);
+        LocalDateTime ultimoHorario = dadosConsultasDto.data().withHour(18);
+        var pacientePossuiOutraConsulta = consultaRespository.existsByPacienteIdAndDataBetween(dadosConsultasDto.idPaciente(),
+                primeiroHorario,
+                ultimoHorario);
+        if (pacientePossuiOutraConsulta) {
+            throw new ValidacaoException("Paciente já possui uma consulta agendada nesse dia.");
+        }
+    }
+
+}

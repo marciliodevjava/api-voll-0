e6f6b4c287e31cc0ae8e2599570cb3edb5fb9b67
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-24 09:53:32 -0300

    Criação de inclass para Validar o HMedico se está ativo

---
+++ src/main/java/br/com/med/voll/api/util/validacoes/ValidadorMedicoAtivo.java
@@ -0,0 +1,28 @@
+package br.com.med.voll.api.util.validacoes;
+
+import br.com.med.voll.api.domain.Medico;
+import br.com.med.voll.api.dto.DadosConsultasDto;
+import br.com.med.voll.api.exception.medico.ValidacaoException;
+import br.com.med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ValidadorMedicoAtivo implements ValidadorAgendamentoDeConsulta {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    public void validar(DadosConsultasDto dados) {
+        if (dados.idMedico() == null) {
+            return;
+        }
+
+        var medicoEstaAtivo = medicoRepository.verificaMedicoAtivo(dados.idMedico());
+        if (!medicoEstaAtivo) {
+            throw new ValidacaoException("Consulta não pode ser agendanda com médico excluido.");
+        }
+    }
+}

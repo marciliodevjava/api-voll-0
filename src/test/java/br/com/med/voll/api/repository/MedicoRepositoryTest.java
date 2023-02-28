109ec09fce706cd8c5c2bbe1e049442285984a6b
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-28 07:53:53 -0300

    Adicionando classes de test

--- src/test/java/br/com/med/voll/api/repository/MedicoRepositoryTest.java
+++ src/test/java/br/com/med/voll/api/repository/MedicoRepositoryTest.java
@@ -1,5 +1,6 @@
 package br.com.med.voll.api.repository;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,9 +20,14 @@ class MedicoRepositoryTest {
     }
 
     @Test
-    void escolherMedicoAlatorio() {
+    @DisplayName("Deve Devolver Null quando o médico não está disponivel na data")
+    void escolherMedicoAlatorioCenarioUm() {
     }
 
+    @Test
+    @DisplayName("Deve Devolver True quando o médico não está disponivel na data")
+    void escolherMedicoAlatorioCenarioDois() {
+    }
     @Test
     void verificaMedicoAtivo() {
     }
